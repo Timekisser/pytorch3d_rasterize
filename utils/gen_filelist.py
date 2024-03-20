4a3df4d599ba45388da3761b735efca6169efff4
wst2001 <weisitong@pku.edu.cn>
2024-03-20 13:08:03 +0800

    update gen filelist

--- utils/gen_filelist.py
+++ utils/gen_filelist.py
@@ -2,22 +2,26 @@ import os
 import wget
 import pandas as pd
 from tqdm import tqdm
+import objaverse
 output_dir = "data/Objaverse"
 
-cap3d_filelist_path = os.path.join(output_dir, "filelist", "Cap3D_automated_Objaverse.csv")
+cap3d_filelist_path = os.path.join(output_dir, "filelist", "Cap3D_automated_Objaverse_highquality.csv")
 
 if not os.path.exists(cap3d_filelist_path):
-    wget.download("https://huggingface.co/datasets/tiange/Cap3D/resolve/main/Cap3D_automated_Objaverse.csv?download=true", out=cap3d_filelist_path)
+    os.system(f"wget https://hf-mirror.com/datasets/tiange/Cap3D/resolve/main/Cap3D_automated_Objaverse_highquality.csv?download=true -O {cap3d_filelist_path}")
 
 cap3d_filelist = pd.read_csv(cap3d_filelist_path, header=None, names=["uid", "description"])
-
 cap3d_uids = cap3d_filelist["uid"].to_list()
+
+objaverse._VERSIONED_PATH = "data/Objaverse/objaverse"
+objaverse_uids = objaverse.load_uids()
+objaverse_glbs = objaverse.load_objects(objaverse_uids, 24)
 
 total_length = 8000000
 uids = []
 for uid in tqdm(cap3d_uids):
     # filename = f"{uid[0]}/{uid}"
-    filename = uid
+    filename = objaverse_glbs[uid].split("/")[4] + "/" + uid
     if os.path.exists(os.path.join(output_dir, "pointcloud_20w", filename, "pointcloud.npz")):
         uids.append(filename)
         if len(uids) > total_length:
