8300839abcf3e859ea62ab259f86211b1d302767
wangjh <artinelysia@gmail.com>
2026-01-28 16:45:16 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# objaverse-pointcloud
-Preprocess objaverse to generate pointcloud
+# Pytorch3d Rasterization to Pointcloud
+
+This repository contains code to rasterize 3D mesh models into multi-view RGB-D images using Pytorch3D, and generate point clouds with color and normal.
+
+## Requirements
+- Python 3.11
+- Pytorch 2.4.1
+Other dependencies can be installed via:
+```
+pip install -r requirements.txt
+```
 
 ## Usage
 
 ```
 sh run.sh
 ```
+
+Parameters in `run.sh` can be modified to fit your needs. Some of the important parameters are:
+- `--dataset`: Dataset name, we provide support for ShapeNet and Objaverse
---
+++ shell/gen_shapenet_pointcloud.sh
@@ -0,0 +1,32 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+N_GPUS=4
+output_dir=data/ShapeNet
+input_dir=/mnt/sdc/weist/data/ShapeNet/
+log_dir=logs
+
+# torchrun \
+# --rdzv_endpoint localhost:26500 \
+# --nproc_per_node=${N_GPUS} \
+
+sudo /home/wangjh/anaconda3/envs/pt3d/bin/python \
+main.py \
+--dataset "ShapeNet" \
+--shapenet_mesh_dir ${input_dir}/ShapeNetCore.v1 \
+--shapenet_filelist_dir ${input_dir}/filelist \
+--output_dir ${output_dir} \
+--pointcloud_folder "pointcloud_render_20w" \
+--image_folder "image_1024_test1" \
+--log_dir ${log_dir} \
+--num_workers 4 \
+--num_points 200000 \
+--image_size 1024 \
+--file_list "train_im_5.txt" "test_im_5.txt" \
+--save_file_type "image" \
+--camera_mode "Perspective" \
+--resume \
+# --camera_mode "Orthographic" \
+# --save_memory
+# --debug \
+# "train_chair.txt" "test_chair.txt" "train_car.txt" "test_car.txt" "train_table.txt" "test_table.txt" "train_rifle.txt" "test_rifle.txt" 
+# --debug \
+######
